75f4875af8b31f59933d4efeb1ae537c20d0cc5b
pabloschonwiesner <pabloschonwiesner@gmail.com>
2019-08-12 01:10:58 -0300

    Subir a heroku

--- server/config.js
+++ server/config.js
@@ -1,1 +1,11 @@
 process.env.PORT =  process.env.PORT || 3000;
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+let urlDB;
+// if( process.env.NODE_ENV === 'dev') {
+//   urlDB = 'mongodb://localhost:27017/cafe';
+// } else {
+  urlDB = 'mongodb+srv://pigmento:KiSXLS8HkQ2M8O2G@cluster0-3pmnb.mongodb.net/cafe'
+// }
+
+process.env.URLDB = urlDB;
